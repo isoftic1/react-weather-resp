e2f23b31ad6296567f8e4a67899f2aa166acc3b0
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-04-12 15:27:41 -0500

    Update WeatherInfo.js

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -8,8 +8,7 @@ export default function WeatherInfo(props) {
       <div className="weatherData">
       <div className="row px-2">
         <div className="col">
-          <h1>
-              <WeatherIcon code={props.data.icon} alt={props.data.description} />
+          <h1><WeatherIcon code={props.data.icon} alt={props.data.description} />
           {props.data.city}
           </h1>
         </div>
