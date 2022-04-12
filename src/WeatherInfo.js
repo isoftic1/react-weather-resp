e20d6591d10a9e61b77afb72b59dd5b27022981f
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-04-12 13:41:19 -0500

    Create WeatherInfo.js

---
+++ src/WeatherInfo.js
@@ -0,0 +1,44 @@
+import React from "react";
+import FormatDate from "./FormatDate";
+import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
+
+export default function WeatherInfo(props) {
+    return (
+      <div className="weatherData">
+      <div className="row px-2">
+        <div className="col">
+          <h1>
+              <WeatherIcon code={props.data.icon} alt={props.data.description} />
+          {props.data.city}
+          </h1>
+        </div>
+      </div>
+      <div className="row px-2">
+        <div className="col">
+          <h2><FormatDate date={props.data.date} /></h2>
+        </div>
+      </div>
+      <div className="row px-2">
+        <div className="col">
+          <WeatherTemperature celsius={props.data.temperature} />
+        </div>
+      </div>
+      <div className="row px-2">
+        <div className="col">
+          <h4 className="text-capitalize">{props.data.description}</h4>
+        </div>
+      </div>
+      <div className="row px-2">
+        <div className="col">
+          <h4>Wind: {Math.round(props.data.wind)} mph</h4>
+        </div>
+      </div>
+      <div className="row px-2">
+        <div className="col">
+          <h4>Humidity: {props.data.humidity}%</h4>
+        </div>
+      </div>
+    </div>
+    );
+}
