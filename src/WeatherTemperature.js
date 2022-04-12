0831272cc914f0e85cea8b4a2aa4491836d96f17
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-04-12 15:16:37 -0500

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function WeatherTemperature(props) {
         return (
         <span className="units">
-            <h3>{Math.round(props.celsius)}˚
-              <span className="conversion">F{" "}</span></h3>
+            <div className="temperature">{Math.round(props.celsius)}˚
+              <span className="conversion">F{" "}</span></div>
           </span>);
 }
