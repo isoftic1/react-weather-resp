14a44b3e39198296764720a59e4c4347b4d712ab
isoftic1 <88169411+isoftic1@users.noreply.github.com>
2022-04-12 13:41:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-        my name is izeta
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid box">
+        <Search defaultCity="Los Angeles"/>
+        <small className="footer">
+          <a href="https://github.com/isoftic1/react-weather-resp.git" target="_blank" rel="noreferrer">
+            Open-source code  {" "}
+            </a>
+          by  {" "}
+          <a href="https://www.linkedin.com/in/isoftic1/" target="_blank" rel="noreferrer">
+           Izeta Softic
+          </a>
+        </small>
+      </div>
+      
     </div>
   );
 }
 
 export default App;
+
+
